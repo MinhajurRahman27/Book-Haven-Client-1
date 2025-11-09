e977ad6fed208ad35569b67aa04b5b009fbcb3ba
Minhajur rahman <minhajurrahman2716@gmail.com>
2025-11-09 14:56:36 +0600

    Added navbar

---
+++ src/main.jsx
@@ -0,0 +1,53 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './Components/Root/Root';
+import Home from './Components/Home/Home';
+import AllBooks from './Components/AllBook/AllBooks';
+import Addbook from './Components/AddBook/Addbook';
+import MyBook from './Components/MyBook/MyBook';
+import Login from './Components/LoginRegister/Login';
+import Register from './Components/LoginRegister/Register';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:Root,
+    children:[
+      {
+        index: true,
+        Component:Home
+      },
+      {
+        path:'allBooks',
+        Component: AllBooks
+      },
+      {
+        path:'addBook',
+        Component:Addbook
+      },
+      {
+        path:'myBooks',
+        Component: MyBook
+      }
+      ,
+      {
+        path:'login',
+        Component: Login
+      },
+      {
+        path: 'register',
+        Component: Register
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
